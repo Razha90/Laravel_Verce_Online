1725c63e3a4c7ef2400274eb53fa8d16b895b1fb
razha90 <razhajamsiksyah@gmail.com>
2023-10-05 08:36:49 -0700

    Update Products

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -26,3 +27,8 @@ Route::delete('/user/{id}', [UserController::class, 'destroy']);
 
 
 
+Route::get('/products', [ProductsController::class, 'index']);
+Route::get('/products/{id}', [ProductsController::class, 'show']);
+Route::post('/products', [ProductsController::class, 'store']);
+Route::put('/products/{id}', [ProductsController::class, 'update']);
+Route::delete('/products/{id}', [ProductsController::class, 'destroy']);
